79fd510314a958b66f27c0b6f75c8cb281e9c23a
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-28 11:12:38 +0700

    updated

--- CreateDatabasesAndTables.sql
+++ CreateDatabasesAndTables.sql
@@ -4,3 +4,35 @@ CREATE DATABASE abc;
 DROP DATABASE abc;
 
 SELECT database();
+
+CREATE DATABASE Instagram;
+CREATE TABLE Tweets (
+	UserName VARCHAR(15),
+    Content VARCHAR(140),
+    Favorites INT
+);
+
+CREATE DATABASE pet_shop;
+USE pet_shop;
+
+CREATE TABLE dogs (
+	Name VARCHAR(15),
+    Breed VARCHAR(50),
+    Age INT
+);
+
+CREATE TABLE cats (
+	Name VARCHAR(15),
+    Breed VARCHAR(50)
+);
+
+DROP TABLE cats;
+
+SHOW tables;
+
+CREATE TABLE pastries (
+	Name VARCHAR(50),
+    Quantity INT
+);
+
+DROP TABLE pastries;
