9b1d27457e8731b074be6e601c2a1acb41780bc0
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-05 09:46:20 +0700

    updated

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -17,4 +17,21 @@ DELIMITER ;
 
 CALL select_employees;
 
+SELECT * FROM salaries;
+SELECT * FROM employees;
+
+DELIMITER $$
+CREATE PROCEDURE select_avg_salary_employees()
+BEGIN
+	SELECT E.emp_no, ROUND(AVG(S.salary),2) AS avg_salary
+    FROM employees AS E
+    LEFT JOIN salaries AS S
+    ON E.emp_no = S.emp_no
+    GROUP BY E.emp_no;
+END$$
+DELIMITER ;
+
+CALL select_avg_salary_employees();
+
+
 
