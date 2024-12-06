ed6cc1c8de2808aeed9051081467ccf0e90f5e70
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-06 11:04:46 +0700

    updated

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -77,3 +77,20 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS select_avg_salary_by_emp_no;
 
 CALL select_avg_salary_by_emp_no(11300);
+
+DELIMITER $$
+CREATE PROCEDURE select_avg_salary_by_emp_no
+	(IN p_emp_no INTEGER, OUT p_avg_salary DECIMAL(10,2))
+BEGIN
+	SELECT ROUND(AVG(S.salary)) INTO p_avg_salary
+    FROM employees AS E
+    LEFT JOIN salaries AS S
+    ON E.emp_no = S.emp_no
+    WHERE E.emp_no = p_emp_no
+    GROUP BY E.emp_no;
+END$$
+DELIMITER ;
+
+SET @avg_salary = 0;
+CALL select_avg_salary_by_emp_no(11300,@avg_salary);
+SELECT @avg_salary AS avg_salary;
