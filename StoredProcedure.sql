2ac6769405dc6f30e2b3f0dff780fdddbd45f754
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-06 10:54:46 +0700

    updated

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -33,5 +33,47 @@ DELIMITER ;
 
 CALL select_avg_salary_employees();
 
+USE employees;
 
+DELIMITER $$
+CREATE procedure select_salaries()
+BEGIN
+	SELECT * FROM salaries
+    LIMIT 1000;
+END$$
+DELIMITER ;
 
+CALL select_salaries;
+
+DROP PROCEDURE IF EXISTS select_salaries;
+
+DELIMITER $$
+CREATE PROCEDURE select_salary_by_emp_no(IN p_emp_no INTEGER)
+BEGIN
+	SELECT E.first_name,E.last_name,S.salary,S.from_date,S.to_date
+    FROM employees AS E
+    LEFT JOIN salaries AS S
+    ON E.emp_no = S.emp_no
+    WHERE E.emp_no = p_emp_no;
+END $$
+DELIMITER ;
+
+CALL select_salary_by_emp_no(10001);
+
+SELECT * FROM employees;
+
+DELIMITER $$
+CREATE PROCEDURE select_avg_salary_by_emp_no(IN p_emp_no INTEGER)
+BEGIN
+	SELECT E.first_name,E.last_name,ROUND(AVG(S.salary),2) AS avg_salary
+    FROM employees AS E
+    LEFT JOIN salaries AS S
+    ON E.emp_no = S.emp_no
+    WHERE E.emp_no = p_emp_no
+    GROUP BY E.first_name,E.last_name;
+END$$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS select_avg_salary_by_emp_no;
+
+CALL select_avg_salary_by_emp_no(11300);
